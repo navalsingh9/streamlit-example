5951a0eca7381abdedac2255939ee67d5d708882
Naval Singh <singhn9@gmail.com>
2022-07-24 20:43:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,18 @@
-a=input('Enter a number')
-b=input('Enter second number')
-print('Sum=',a+b)
+
+def main():
+    st.title('Adding two numbers')
+    st.warning('Only Enter Numeric Values in the Following Fields')
+    a = st.text_input("Enter first number")
+    b = st.text_input("Enter second number")
+    submit = st.button('Add numbers')
+    if submit: 
+        if a and b:
+            with st.spinner('Calculating...'):
+                time.sleep(2)
+                addition = a+b
+                st.info(f"Your **sum** of {a} and {b} is {addition}")
+        else:
+            st.error('Please Enter All the Details')
+@st.cache
+if __name__ == '__main__':
+    main()
